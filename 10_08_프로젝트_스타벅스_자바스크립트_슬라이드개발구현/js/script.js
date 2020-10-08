6c1169728baf3474348ff44b735e79528d06bba8
Elly Sohye Park <63501197+ps9610@users.noreply.github.com>
2020-10-08 15:30:33 +0900

    제이쿼리 완료

--- 10_08_프로젝트_스타벅스_자바스크립트_슬라이드개발구현/js/script.js
+++ 10_08_프로젝트_스타벅스_자바스크립트_슬라이드개발구현/js/script.js
@@ -1,5 +1,21 @@
-// 1. 선언적 함수 (이름있는 함수) -> 이름을 불러줘야 실행이 됨
 
+/* 
+// 윈도우가 로드된 후에 이벤트 리스너(function) 함수 실행
+// window.onload = function(){}
+// 돔트리(DOM TREE) 구조(Structure)를 읽어 들이고 난 후, 실행할 수 있도록 합니다. = 자바스크립트를 어디다가써도 실행가능하게해줌
+/*
+window.onload = function(){
+    
+    //fnFirst('window.onload 실행','바보'); //전달인자(전달인수)=argument | 매개변수한테 전달하는 값=매개변수가없으면 전달인자도 쓰나마나임
+    
+    var divBtn = document.querySelector('#scriptWrap');
+    divBtn.onclick = function(){
+        fnFirst('온로드 후 버튼 클릭 이벤트 리스너 값입니다.');
+    }
+}  
+*/
+
+// 1. 선언적 함수 (이름있는 함수) -> 이름을 불러줘야 실행이 됨
 /*
 // fnFirst('기본함수 호출 실행','박소혜'); //위에서 호출(가능)
 
@@ -13,7 +29,14 @@
 //1.👆고객부름        2.👆쿠폰받음        //아래에서 호출     
 */
 //이름있는(선언적)함수 특징 1) 함수 선언문 위/아래 상관없이 호출실행 가능;
-
+/* 
+//버튼(요소Element) 클릭 이벤트
+    var divBtn = document.querySelector('#scriptWrap');
+    
+     divBtn.onclick  = function(){
+         fnFirst();
+     }
+*/
 /*
 //////2. 리터럴(이름없는) 함수 //////
 //특징 1) 변수로 대입하는 함수
@@ -33,32 +56,33 @@ unNameFn('http://www.tjoeun.co.kr'); //argument(전달인자)
 //unNameFn이라는 함수를 호출할 때 더조은 홈페이지가 z한테 전달이 됨
 */
 
-
 //////3. 즉시 실행 함수 //////
 
-
+/*
 (function(){
     alert('즉시 실행 함수');
 })(); //이 괄호가 즉시 실행하게 해줌
 
-(function(babo,jQuery,win/*parameter(매개변수)*/){
-    console.log(babo,jQuery,win) //함수에서받은 매개변수는 함수 내에서 사용할수잇음, 서로 짝이 안 맞으면 undefined
-})('전달인자값1','전달인자값2'/*argumrnt(전달인자)*/); 
+(function(babo,jQuery,win/*parameter(매개변수)*///){
+    //console.log(babo,jQuery,win) //함수에서받은 매개변수는 함수 내에서 사용할수잇음, 서로 짝이 안 맞으면 undefined
+//})('전달인자값1','전달인자값2'/*argumrnt(전달인자)*/); 
 
 //제이쿼리 실행 함수
 //반드시 오픈소스가 있어야 사용이 가능
-jQuery(function(){ //다음의 것들을 제이쿼리로 실행합니다.
+/*jQuery(function(){ //다음의 것들을 제이쿼리로 실행합니다.
     alert('제이쿼리 실행 경고창!!!!!!!!!!!!!!!!!!!');
-    /*⭐*/jQuery('#scriptWrap').html('<h1>제이쿼리 출력문</h1>'); //css규칙은 그대로 지킴.
-    $('#scriptWrap').html('<h2>제이쿼리 출력문</h2>'); //제이쿼리 내의 변수는 중복이 되니까 조심
+    /*⭐*//*jQuery('#scriptWrap').html('<h1>제이쿼리 출력문</h1>'); //css규칙은 그대로 지킴.
+    /*$('#scriptWrap').html('<h2>제이쿼리 출력문</h2>'); //제이쿼리 내의 변수는 중복이 되니까 조심
 });
 $(function(){
     alert('제이쿼리 실행문 $사용(jQuery 대신 문자사용)')
     $('#scriptWrap').text('<h3><i>제이쿼리는 출력문이 2가지다. 1)html()태그사용가능 2)text()오로지(Only)텍스트만</i></h3>')// text: 태그인식못함 / html 태그인식가능
-    /*⭐*/document.write('<h1>자바스크립트 코딩 출력문~</h1>'); //웹에 직접 코딩하는거라 직접 태그 가능
+    /*⭐*//*document.write('<h1>자바스크립트 코딩 출력문~</h1>'); //웹에 직접 코딩하는거라 직접 태그 가능
       console.log('콘솔창 출력 오류검출 디버깅용 유효성검사') 
       alert('경고창!!!') 
-}); /* 어떤 값을 받고 되돌려주는 함수 = 콜백함수 = 이벤트핸들러 */
+}); //어떤 값을 받고 되돌려주는 함수 = 콜백함수 = 이벤트핸들러 
+*/
+
 
 /*
 // 입력 방법
@@ -73,28 +97,48 @@ $(function(){
 3) $(function(){
 
 });
+4) $(function(){
+
+})();
 */
 
-/* 
-//버튼(요소Element) 클릭 이벤트
-    var divBtn = document.querySelector('#scriptWrap');
-    
-     divBtn.onclick  = function(){
-         fnFirst();
-     }
-*/
-
-// 윈도우가 로드된 후에 이벤트 리스너(function) 함수 실행
-// window.onload = function(){}
-// 돔트리 구조를 읽어 들이고 난 후, 실행할 수 있도록 합니다.
+// 즉시 실행함수로 제이쿼리의 단점 충돌을 차단
+// 변수 중복 사용 공용 차단
 /*
-window.onload = function(){
+// 1) 
+ ();
+// 2) 
+ (function(){});
+// 3) 
+ (function(){
+ 
+ });
+ */
 
-    //fnFirst('window.onload 실행','바보'); //전달인자(전달인수)=argument | 매개변수한테 전달하는 값=매개변수가없으면 전달인자도 쓰나마나임
+// 4) 
+/*
+(function($,window,document,undefined/*parameter(매개변수)*///){
 
-    var divBtn = document.querySelector('#scriptWrap');
-        divBtn.onclick = function(){
-            fnFirst('온로드 후 버튼 클릭 이벤트 리스너 값입니다.');
-        }
-}  
+    //console.log( $ ); //-> ƒ (a,b){return new n.fn.init(a,b)} = 제이쿼리(이제부터 제이쿼리 사용이 가능)
+    //$('#scriptWrap').html('<h1>제이쿼리 즉시 실행문</h1>')
+    //$('#scriptWrap').css({color:'#c00',fontSize:50,textAlign:'center'})
+    //$('#scriptWrap').css({color:'#c00','font-size':50,'text-align':'center'}) 이렇게 써도 됨
+ 
+//})(jQuery,window,document/* argument(전달인자) */);
+*/
+
+
+
+
+
+/* ㅜㅜ */
+(function(jQuery,window,document,undefined){
+
+})(jQuery,window,document)
+
+/*
+제이쿼리 기본형
+(function($){
+
+})(jQuery);
 */
