557bc89f0790534818e72f4afdfcf6b9e4b7d166
Elly Sohye Park <63501197+ps9610@users.noreply.github.com>
2020-10-08 18:27:03 +0900

    과제생성 / css초기화하는것

---
+++ 10_08_프로젝트_스타벅스_섹션1_애니메이션 [과제]/js/section1.js
@@ -0,0 +1,7 @@
+(function($,window,document,undefined){
+    function fnSection1(){
+        $('.ani-1').stop().animate({opacity:0},300).animate({opacity:1},500);
+    };
+
+    fnSection1();
+}) (jQuery,window,document);
