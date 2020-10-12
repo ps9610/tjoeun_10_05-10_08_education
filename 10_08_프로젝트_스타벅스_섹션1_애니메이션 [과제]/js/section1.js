1b03647d397b00c2d5eb61cedd8aae27d3fb873a
Elly Sohye Park <63501197+ps9610@users.noreply.github.com>
2020-10-12 11:14:45 +0900

    제이쿼리완료

--- 10_08_프로젝트_스타벅스_섹션1_애니메이션 [과제]/js/section1.js
+++ 10_08_프로젝트_스타벅스_섹션1_애니메이션 [과제]/js/section1.js
@@ -1,7 +1,24 @@
 (function($,window,document,undefined){
-    function fnSection1(){
-        $('.ani-1').stop().animate({opacity:0},300).animate({opacity:1},500);
-    };
 
-    fnSection1();
-}) (jQuery,window,document);
+    $('.ani').css({opacity:0},500);
+
+    function fnSection1(){
+        $('.ani-1').stop().animate({opacity:1},500, function(){
+            $('.ani-2').stop().animate({opacity:1},500, function(){
+                $('.ani-3').stop().animate({opacity:1},500, function(){
+                    $('.ani-4').stop().animate({opacity:1}, 500, function(){
+                        $('.ani-5').stop().animate({opacity:1}, 500, function(){
+                            $('.ani-6').stop().animate({opacity:1}, 500);
+                        });
+                    });
+                });
+            });
+        });
+    }
+
+    fnSection1()
+
+
+
+
+})(jQuery,window,document);
